bbe40e0dcfb6a8c8f5675e3fed2275fe8a3af79b
Richard <duddydad490@gmail.com>
2024-04-05 18:42:15 +0700

    Swagger DOc

--- app.js
+++ app.js
@@ -34,9 +34,16 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+//swagger-ui
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 app.get("/", (req, res) => {
   res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
 });
+
+app.use("/api-docs", swaggerUI.serve , swaggerUI.setup(swaggerDocument));
 // routes
 app.use("/api/v1/auth", AuthRouter);
 app.use("/api/v1/jobs", auth, JobRouter);
